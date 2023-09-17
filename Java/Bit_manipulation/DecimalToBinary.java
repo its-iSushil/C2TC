81e13b36b34e6ee0c7133ca316f964b5f70f55a8
Sushilkumar Patil <137991259+its-iSushil@users.noreply.github.com>
2023-09-17 15:40:35 -0400

    Update DecimalToBinary.java

--- Java/Bit_manipulation/DecimalToBinary.java
+++ Java/Bit_manipulation/DecimalToBinary.java
@@ -2,13 +2,27 @@ package Bit_manipulation;
 
 import java.util.Scanner;
 
+//program to convert decimal to binary.
+
 public class DecimalToBinary 
 {
     public static void main(String[] args)
     {
-        
-        System.out.println(5<<2);
- 
+        System.out.println("Enter a decimal number:");
+        Scanner scan=new Scanner(System.in);
+        int decimal=scan.nextInt();
 
+        System.out.println("Binary number of "+decimal+" is "+returnBinary(decimal));
+
+    }
+
+    private static int returnBinary(int decimal) {
+        int pow=0,bin=0,rem=0;
+        while(decimal>0){
+            rem=decimal%2;
+            bin=bin+(int)(rem*Math.pow(10, pow));
+            pow++; decimal/=2;
+        }
+        return bin;
     }
 }
