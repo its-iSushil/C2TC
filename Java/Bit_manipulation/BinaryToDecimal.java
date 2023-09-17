2b59f49fa4d7323b55011c9b3b4cede878cd2cc3
Sushilkumar Patil <137991259+its-iSushil@users.noreply.github.com>
2023-09-17 15:39:52 -0400

    Update BinaryToDecimal.java

--- Java/Bit_manipulation/BinaryToDecimal.java
+++ Java/Bit_manipulation/BinaryToDecimal.java
@@ -1,23 +1,25 @@
 package Bit_manipulation;
-
 import java.util.Scanner;
 
-public class BinaryToDecimal
-{
-    public static void main(String[] args) 
-    {
-        System.out.println("Enter a binary number :");
-        Scanner scan=new Scanner(System.in);
-        int n=scan.nextInt();
-        int rem=0,temp=0;
-        double dn=1;
-        while(n>0)
-        {
-            temp=n%10;
-            dn=temp*(Math.pow(2, rem));
-            n=n/10;
-            rem++;
+//Program to convert binary number to decimal number
+public class Binary_To_Decimal {
+
+    private static int returnDecimal(int binary){
+        int decimal=0;
+        int pow=0;
+        while(binary>0){
+            int lastDigit=binary%10;
+            decimal=(int) (decimal+(lastDigit*Math.pow(2, pow)));
+            pow++;
+            binary/=10;
         }
-        System.out.println("Decimal no is : "+dn);
+        return decimal;
+    }
+    public static void main(String[] args) {
+        System.out.println("Enter any binary number:");
+        Scanner scan=new Scanner(System.in);
+        int binary=scan.nextInt();
+        
+        System.out.println("Decimal number of "+binary+" is "+returnDecimal(binary));
     }
 }
